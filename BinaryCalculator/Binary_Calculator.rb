c0932407b7b9db556b8f0c2af51df5922e5e4133
Mr_CyberLord <cladangab@gmail.com>
2015-06-01 11:28:44 -0300

    Revert "Update 3"
    This reverts commit 99f909141e57473a147a44bc056e5af445b9b239.

--- BinaryCalculator/Binary_Calculator.rb
+++ BinaryCalculator/Binary_Calculator.rb
@@ -116,8 +116,9 @@ end
 
 class Calculator
 	def sum(value1, value2)
-		firstValue = value1
-		secondValue = value2
+		carry = []
+		firstValue = firstValue.split(//)
+		secondValue = secondValue.split(//)
 
 		while firstValue.length > secondValue.length
 			secondValue = "0" + secondValue
@@ -126,166 +127,41 @@ class Calculator
 		while secondValue.length > firstValue.length
 			firstValue = "0" + firstValue
 		end
-		firstValue = firstValue.to_s.split(//)
-		secondValue = secondValue.to_s.split(//)
 
 		firstValue = firstValue.reverse
 		secondValue = secondValue.reverse
 
-		#Aqui termina o algoritmo que inverte o binário
-
-		carry = ["0"]
-
-		result = ""
-
-		firstValue.each_with_index do |bit, i|
-			if carry[i] == "1"
-				if bit == "1"
-					bit = "0"
-					carry << "1"
-				elsif 
-					bit = "1"
-				end
-			end
-			if bit == "1" && secondValue[i] == "1"
-				bit = "0"
-				carry << "1"
-			elsif bit == "1" || secondValue[i] == "1"
-				bit = "1"
-				if i < firstValue.length-1
-					carry << "0"
-				end
-			else
-				bit = "0"
-				if i < firstValue.length-1
-					carry << "0"
-				end
-			end
-
-			result = bit + result
-		end
-
-		if carry.length > firstValue.length
-			result = carry[carry.length - 1] + result
-		end
-
-		return result
+		puts firstValue.to_s + "	||	" + secondValue.to_s
 	end
 
 	def subtraction(value1, value2)
-		firstValue = value1
-		secondValue = value2
-
-		while firstValue.length > secondValue.length
-			secondValue = "0" + secondValue
-		end
-
-		while secondValue.length > firstValue.length
-			firstValue = "0" + firstValue
-		end
-		firstValue = firstValue.to_s.split(//)
-		secondValue = secondValue.to_s.split(//)
-
-		firstValue = firstValue.reverse
-		secondValue = secondValue.reverse
-
-		#Aqui termina o algoritmo que inverte o binário
-
-		result = ""
-
-		carry = [0]
-
-		firstValue.each_with_index do |bit, i|
-			changeCarry =  true
-			if carry[i] == "1"
-				if bit == "1"
-					bit = "0"
-				elsif 
-					bit = "1"
-					carry << "1"
-					changeCarry = false
-				end
-			end
-
-			if bit == "0" && secondValue[i] == "1"
-				bit = "1"
-				carry << "1"
-			elsif bit == "1" && secondValue[i] == "0"
-				bit = "1"
-				if changeCarry
-					carry << "0"
-				end
-			else
-				bit = "0"
-				if changeCarry
-					carry << "0"
-				end
-			end
-
-			result = bit + result
-
-		end
-
-		return result
-
+		
 	end
 
 	def multiplication(value1, value2)
-		firstValue = value1
-		secondValue = value2
-
-		while firstValue.length > secondValue.length
-			secondValue = "0" + secondValue
-		end
-
-		while secondValue.length > firstValue.length
-			firstValue = "0" + firstValue
-		end
-		firstValue = firstValue.to_s.split(//)
-		secondValue = secondValue.to_s.split(//)
-
-		firstValue = firstValue.reverse
-		secondValue = secondValue.reverse
-
-		#Aqui termina o algoritmo que inverte o binário
-
-		values = []
-
-		result = "0"
-
-		secondValue.each_with_index do |bit, i|
-			firstValue.each_with_index do |char, p|
-				values[i] = (bit.to_i * char.to_i).to_s + values[i].to_s
-			end
-		end
-
-		values.each_with_index do |val, i|
-			zeros = ""
-			while zeros.length < i
-				zeros += "0"
-			end
-			val += zeros
-
-			result = sum(result, val)
-		end
-
-		return result
-
+		
 	end
 	def division(value1, value2)
+		
+	end
 
-		q = "0"
-		while value1.to_i >= value2.to_i
-			value1 = subtraction(value1,value2)
-			q = sum(q,"1")
+	def search_operation(value1, value2, operation)
+		case object
+			when "+"
+				puts sum(value1,value2)
+			when "-"
+				
+			when "*"
+				
+			when "/"
+				
 		end
-		return q
 	end
 end
 
 
 puts "Welcome to..."
-puts "-=[Binary Calculator v1.0]=-"
+puts "-=[Binary Conversor v0.35]=-"
 puts "	by Gabriel de Souza"
 puts "\nThank you for your preference!"
 puts "\n\nType exit to close this application."
@@ -293,7 +169,6 @@ puts "\n\nType exit to close this application."
 conversor = Conversor.new
 
 calculator = Calculator.new
-
 
 enterType = nil
 
@@ -307,27 +182,20 @@ operation = nil
 
 while enterType.nil?
 	if firstValue != nil && secondValue != nil
-		puts "\n\nFirst value of operation:\n" + firstValue.to_s
-		puts "Seconde value of operation:\n" + secondValue.to_s
+		puts "\n\nPrimeiro Valor da operacao: " + firstValue.to_s
+		puts "Segundo Valor da operacao: " + secondValue.to_s
 		puts "\nYour operation is: " + operation
-
-		final_result = ""
 
 		case operation
 			when "+"
-				final_result = calculator.sum(firstValue, secondValue)
+				puts "\n\nYour final result is: " + "a" + "\n__________________________________________________\n__________________________________________________\n\n"
 			when "-"
-				final_result = calculator.subtraction(firstValue, secondValue)
+				puts "\n\nYour final result is: " + "b" + "\n__________________________________________________\n__________________________________________________\n\n"
 			when "*"
-				final_result = calculator.multiplication(firstValue, secondValue)
+				puts "\n\nYour final result is: " + "c" + "\n__________________________________________________\n__________________________________________________\n\n"
 			when "/"
-				puts "\n\nYour final result is: " + calculator.division(firstValue, secondValue) + "\n__________________________________________________\n__________________________________________________\n\n"
+				puts "\n\nYour final result is: " + "d" + "\n__________________________________________________\n__________________________________________________\n\n"
 		end
-
-		puts "\n\nYour final result is: " + final_result.to_s + "\n__________________________________________________\n__________________________________________________\n\n"
-
-		#puts "Your result in decimal is: " + conversor.BinToDec(final_result).to_s
-		#puts "Your result in hexadecimal is: " + conversor.BinToHexDec(final_result).to_s + "\n\n"
 
 		firstValue = nil
 		operation = nil
@@ -376,15 +244,11 @@ while enterType.nil?
 									puts "\n\nYour value is: " + value
 									puts "Your value in Binary is: " + conversor.DecToBin(value).to_s
 									puts "Your value in Hexadecimal is: " + conversor.DecToHexDec(value).to_s
-
-									firstValue = conversor.DecToBin(value)
 								elsif secondValue.nil?
 									secondValue = conversor.DecToBin(value)
 									puts "\n\nYour value is: " + value
 									puts "Your value in Binary is: " + conversor.DecToBin(value).to_s
 									puts "Your value in Hexadecimal is: " + conversor.DecToHexDec(value).to_s
-
-									secondValue = conversor.DecToBin(value)
 								end
 							else
 								puts "\n\nPlease, enter a valid value in decimal!"
@@ -402,15 +266,11 @@ while enterType.nil?
 									puts "\n\nYour value is: " + value
 									puts "Your value in Decimal is: " + conversor.HexDecToDec(value).to_s
 									puts "Your value in Binary is: " + conversor.HexDecToBin(value).to_s
-
-									firstValue = conversor.HexDecToBin(value)
 								elsif secondValue.nil?
 									secondValue = conversor.HexDecToBin(value)
 									puts "\n\nYour value is: " + value
 									puts "Your value in Decimal is: " + conversor.HexDecToDec(value).to_s
 									puts "Your value in Binary is: " + conversor.HexDecToBin(value).to_s
-
-									secondValue = conversor.HexDecToBin(value)
 								end
 							else
 								puts "\n\nPlease, enter a valid value in hexadecimal!"
